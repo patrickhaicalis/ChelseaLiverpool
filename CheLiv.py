a70a5b147a1086b718f27a0b65a3a88ade6f88ef
patrickhaicalis <54026477+patrickhaicalis@users.noreply.github.com>
2020-07-25 00:09:36 +0100

    Update CheLiv.py

--- CheLiv.py
+++ CheLiv.py
@@ -4,14 +4,12 @@ import numpy as np
 
 df = pd.read_csv('CheLiv.csv')
 
-plt.axis([0, 95, 0, 3])
-
 for name, df in df.groupby('Team'):
     plt.plot(df['Minute'], df['CumulativexG'], label=name, drawstyle='steps-post')
 
 plt.legend()
 plt.title('Chelsea vs Livepool xG')
-plt.axis([0, 95, 0, 3])
+plt.axis([0, max(df['Minute']), 0, 3])
 plt.xticks(np.arange(0, max(df['Minute']), step=15))
 plt.xlabel('Minutes Played')
 plt.ylabel('Expected Goals')
